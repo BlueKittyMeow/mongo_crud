448709f99e74dddb3efc6822b1f202f8fcf1e4e1
BlueKittyMeow <82776271+BlueKittyMeow@users.noreply.github.com>
2023-05-23 12:33:50 -0400

    add crud for students and new rooms

--- app.js
+++ app.js
@@ -41,13 +41,49 @@ app.get('/api/rooms/number/:room_number', async (req, res) => {
   try {
     const { room_number } = req.params;
     const roomsCollection = client.db("CirculationApp").collection("spaces");
+    const studentsCollection = client.db("CirculationApp").collection("students");
+
     const room = await roomsCollection.findOne({ room_number: room_number });
+    if (!room) {
+      res.status(404).send('Room not found');
+      return;
+    }
+
+    const studentIds = room.assignees.map(id => ObjectId(id));
+    const students = await studentsCollection.find({ _id: { $in: studentIds } }).toArray();
+
+    // Add students data to the room data
+    room.students = students;
+
     res.json(room);
   } catch (err) {
     console.error(err);
     res.status(500).send('Internal server error');
   }
 });
+
+
+// Check if a string is a valid ObjectId
+function isValidObjectId(id) {
+  return /^[0-9a-fA-F]{24}$/.test(id);
+}
+
+//API endpoint for updating room data
+app.put('/api/rooms/number/:room_number', async (req, res) => {
+  try {
+      const { room_number } = req.params;
+      const { room_status, admin_notes, room_notes } = req.body;
+
+      const roomsCollection = client.db("CirculationApp").collection("spaces");
+      await roomsCollection.updateOne({ room_number: room_number }, { $set: { room_status, admin_notes, room_notes } });
+
+      res.sendStatus(200);
+  } catch (err) {
+      console.error(err);
+      res.status(500).send('Internal server error');
+  }
+});
+
 
 
 // Check if a string is a valid ObjectId
@@ -90,6 +126,89 @@ app.post('/api/students', async (req, res) => {
     res.status
   }
 });
+
+// API endpoint for adding a new student
+app.post('/api/students', async (req, res) => {
+  const student = req.body;
+  try {
+      const studentsCollection = client.db("CirculationApp").collection("students");
+      const result = await studentsCollection.insertOne(student);
+      res.sendStatus(200);
+  } catch (err) {
+      console.error(err);
+      res.status(500).send('Internal server error');
+  }
+});
+
+
+
+
+
+// API endpoint for adding a new room
+app.post('/api/rooms', async (req, res) => {
+  const room = req.body;
+  try {
+    const roomsCollection = client.db("CirculationApp").collection("spaces");
+    const result = await roomsCollection.insertOne(room);
+    res.sendStatus(200);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal server error');
+  }
+});
+
+// API endpoint for searching students
+app.get('/api/students/search', async (req, res) => {
+  const { type, term } = req.query;
+  try {
+    const studentsCollection = client.db("CirculationApp").collection("students");
+    let query = {};
+
+    if (type === 'id') {
+      query.pratt_id = term;
+    } else if (type === 'email') {
+      query.email = term;
+    } else if (type === 'name') {
+      query.$or = [{ first_name: term }, { legal_first_name: term }, { last_name: term }];
+    }
+
+    query.enrollment_status_current = true;
+
+    const students = await studentsCollection.find(query).limit(25).toArray();
+    res.json(students);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal server error');
+  }
+});
+
+// API endpoint for assigning a student to a room
+app.put('/api/rooms/:room_id/assignees/:student_id', async (req, res) => {
+  const { room_id, student_id } = req.params;
+  try {
+    const roomsCollection = client.db("CirculationApp").collection("spaces");
+    const result = await roomsCollection.updateOne({ _id: ObjectId(room_id) }, { $push: { assignees: student_id } });
+    res.sendStatus(200);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal server error');
+  }
+});
+
+app.get('/api/room_types', async (req, res) => {
+  try {
+    const roomTypesCollection = client.db("CirculationApp").collection("room_types");
+    const roomTypes = await roomTypesCollection.find({}).toArray();
+    res.json(roomTypes);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal server error');
+  }
+});
+
+
+
+
 
 
 
