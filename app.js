46c2ff269d277c15789dce564ca85390434f1acb
BlueKittyMeow <82776271+BlueKittyMeow@users.noreply.github.com>
2023-06-05 13:03:43 -0400

    update app.js
    added port details

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const HttpsProxyAgent = require('https-proxy-agent');
 
 const app = express();
 app.use(express.json());
-const port = 3000;
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('App is running on http://localhost:' + port);
+});
 
 // Get your QuotaGuard URL from the environment variable
 const QGTunnel = process.env.QUOTAGUARDSTATIC_URL;
